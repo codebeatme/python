75be7c0acb6b9a633a43ebbbc467c7face27d385
我被代码海扁 <beanme@qq.com>
2023-02-27 13:48:49 +0800

    更新 method.py

--- src/advanced/method.py
+++ src/advanced/method.py
@@ -1,25 +1,40 @@
+###
 class Student:
 
-    def __init__(self, n, a):
+    def __init__(self, n, a, s):
         self.name = n
         self.age = a
+        self.scores = s
 
+    def sum_score(self):
+        total = 0
+
+        for score in self.scores:
+            total += int(score)
+
+        return total
+
+###
 students = []
 
 while True:
-    text = input('请输入姓名和年龄，使用空格分割：')
+    text = input('请输入姓名和年龄以及成绩，使用空格分割：')
 
     if text == 'n':
         break
 
+    ###
     args = text.split(' ')
 
     input_name = args[0]
     input_age = int(args[1])
+    input_scores = args[2:]
 
-    student = Student(input_name, input_age)
+    student = Student(input_name, input_age, input_scores)
 
     students.append(student)
 
+###
 for s in students:
-    print(f'学生{s.name} 年龄{s.age}')
+    total_score = s.sum_score()
+    print(f'学生{s.name} 年龄{s.age} 成绩{total_score}')
